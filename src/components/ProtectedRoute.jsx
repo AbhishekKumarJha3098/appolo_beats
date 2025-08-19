2b3574e54b6275e5f00ed83cf12de4377f0145e0
Abhishek Kumar Jha <87441996+AbhishekKumarJha3098@users.noreply.github.com>
2025-08-20 02:04:54 +0530

    Delete src directory

--- src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { useSelector } from 'react-redux'
-import { Navigate, Outlet } from 'react-router-dom'
-
-
-export default function ProtectedRoute() {
-const token = useSelector(s => s.auth.token)
-return token ? <Outlet /> : <Navigate to="/login" replace />
-}
